90cbcd29517bbb2ebc46ad016b5db65e89869f01
urvish1920 <urvishpatel192011@gmail.com>
2024-01-17 16:19:47 +0530

    app it perform all operation

---
+++ app.java
@@ -0,0 +1,10 @@
+public class app{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 5;
+        System.out.println("Sum of two number : "+(a+b));
+        System.out.println("Sum of two number : "+(a-b));
+        System.out.println("Sum of two number : "+(a*b));
+        System.out.println("Sum of two number : "+(a/b));
+    }
+}
--- new.java
+++
@@ -1,7 +0,0 @@
-public class sub{
-    public static void main(String[] args){
-        int a = 5;
-        int b = 5;
-        System.out.println("Sum of two number : "+(a-b));
-    }
-}
